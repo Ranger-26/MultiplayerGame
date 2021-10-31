64f1ac35675a7e1674bebb289719c2dc43658fcc
smohan67 <siddhu.mohan6@gmail.com>
2021-10-30 22:31:20 -0500

    add main menu

---
+++ MultiplayerGame/Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using Lobby;
+using Mirror;
+using UnityEngine;
+
+namespace MainMenu
+{
+    public class MainMenu : MonoBehaviour
+    {
+        [SerializeField] private NetworkManagerLobby networkManager;
+
+        [Header("UI")] [SerializeField] private GameObject landingPagePanel;
+
+        public void HostLobby()
+        {
+            networkManager.StartHost();
+            
+            landingPagePanel.SetActive(false);
+        }
+    }
+}
