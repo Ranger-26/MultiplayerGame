4a39180d30c416f2442c7d240160e04dd3863b3f
RedRanger26 <61168189+RedRangerr@users.noreply.github.com>
2021-11-01 13:48:57 -0500

    Revert "Revert "add main menu""
    This reverts commit 4f2bc52da34eb83ca8aed39ec4336a7775422890.

---
+++ MultiplayerGame/Assets/Scripts/MainMenu/JoinLobbyMenu.cs
@@ -0,0 +1,54 @@
+﻿using Lobby;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MainMenu
+{
+    public class JoinLobbyMenu : MonoBehaviour
+    {
+        [SerializeField] private NetworkManagerLobby networkManager;
+
+        [Header("UI")]
+        [SerializeField] private GameObject landingPagePanel;
+        [SerializeField] private TMP_InputField ipAddressInputField;
+        [SerializeField] private Button joinButton;
+
+        private void OnEnable()
+        {
+            NetworkManagerLobby.OnClientConnected += HandleClientConnected;
+            NetworkManagerLobby.OnClientDisconnected += HandleClientDisconnected;
+        }
+
+        private void OnDisable()
+        {
+            NetworkManagerLobby.OnClientConnected -= HandleClientConnected;
+            NetworkManagerLobby.OnClientDisconnected -= HandleClientDisconnected;
+        }
+
+        public void JoinLobby()
+        {
+            string ipAddress = ipAddressInputField.text;
+
+            networkManager.networkAddress = ipAddress;
+            networkManager.StartClient();
+
+            joinButton.interactable = false;
+        }
+
+        private void HandleClientConnected()
+        {
+            Debug.Log("Client has connected.");
+            joinButton.interactable = true;
+
+            gameObject.SetActive(false);
+            landingPagePanel.SetActive(false);
+        }
+
+        private void HandleClientDisconnected()
+        {
+            Debug.Log("Client has disconnected.");
+            joinButton.interactable = true;
+        }
+    }
+}
---
+++ MultiplayerGame/Assets/Scripts/MainMenu/PlayerNameInput.cs
@@ -0,0 +1,45 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MainMenu
+{
+    public class PlayerNameInput : MonoBehaviour
+    {
+        [Header("UI")]
+        [SerializeField] private TMP_InputField nameInputField;
+        [SerializeField] private Button continueButton;
+
+        public static string DisplayName { get; private set; }
+
+        private const string PlayerPrefsNameKey = "PlayerName";
+
+        private void Start()
+        {
+            SetUpInputField();
+        }
+
+        private void SetUpInputField()
+        {
+            if (!PlayerPrefs.HasKey(PlayerPrefsNameKey)) { return; }
+
+            string defaultName = PlayerPrefs.GetString(PlayerPrefsNameKey);
+
+            nameInputField.text = defaultName;
+
+            SetPlayerName(defaultName);
+        }
+
+        public void SetPlayerName(string newname)
+        {
+            continueButton.interactable = !string.IsNullOrEmpty(newname);
+        }
+
+        public void SavePlayerName()
+        {
+            DisplayName = nameInputField.text;
+
+            PlayerPrefs.SetString(PlayerPrefsNameKey, DisplayName);
+        }
+    }
+}
